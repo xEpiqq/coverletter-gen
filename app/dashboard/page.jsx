8f9a64c3361cff5328c7cd28aac98de5d22dcb10
trytryty1 <58746198+trytryty1@users.noreply.github.com>
2023-03-24 17:46:57 -0600

    finished dashboard ui

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -24,7 +24,7 @@ export default function Page() {
   };
 
   const createCoverLetter = (e) => {
-    // TODO: Create cover letter
+    setCoverLetterOptions([...coverLetterOptions, "New Cover Letter"]);
   };
 
   const generateCoverLetter = (e) => {
@@ -35,10 +35,12 @@ export default function Page() {
     <div className={s.page}>
       <div className={s.navbar}>
         <div className={s.navbar_left}>
-          <img src="/logo_background.svg" alt="logo" />
+          <img src="/logo_green.svg" alt="logo" />
         </div>
         <div className={s.navbar_center}>
+          <img src="/double_arrow_left.svg" alt="logo" />
           <h1>COVER LETTER GENERATOR</h1>
+          <img src="/double_arrow_right.svg" alt="logo" />
         </div>
         <div className={s.navbar_right}>USER_PLACEHOLDER</div>
       </div>
@@ -68,8 +70,14 @@ export default function Page() {
           <div className={s.content_left_bottom}>
             <div className={s.container}>
               <hr />
-              <button className={s.subscription_button}>Subscription</button>
-              <button className={s.logout_button}>Logout</button>
+              <button className={s.subscription_button}>
+                <img src="/subscription_icon.svg" alt="logo" />
+                Subscription
+              </button>
+              <button className={s.logout_button}>
+                <img src="/logout_icon.svg" alt="logo" />
+                Logout
+              </button>
             </div>
           </div>
         </div>
@@ -101,16 +109,41 @@ export default function Page() {
         </div>
         <div className={s.content_right}>
           <h2>Details</h2>
-          <label htmlFor="Job input">Job</label>
-          <input id="Job input" className={s.content_right_input} />
-          <label htmlFor="Company input">Company</label>
-          <input id="Company input" className={s.content_right_input} />
-          <label htmlFor="Location input">Location</label>
-          <input id="Location input" className={s.content_right_input} />
-          <label id="Job Description input">Paste the job description here</label>
-          <textarea id="Job Description input" className={s.content_right_input_textbox} />
-          The creativity meter
-          <div className={s.content_right_meter} />
+          <div className={s.input_container}>
+            <label htmlFor="Job input">Job Title</label>
+            <input id="Job input" className={s.content_right_input} />
+          </div>
+          <div className={s.input_container}>
+            <label htmlFor="Company input">Company</label>
+            <input id="Company input" className={s.content_right_input} />
+          </div>
+          <div className={s.input_container}>
+            <label htmlFor="Location input">Location</label>
+            <input id="Location input" className={s.content_right_input} />
+          </div>
+          <div className={s.input_container_big}>
+            <label id="Job Description input">
+              Paste the job description here
+            </label>
+            <textarea
+              id="Job Description input"
+              className={s.content_right_input_textbox}
+            />
+          </div>
+          <div className={s.input_container}>
+            <label htmlFor="Creativity input">The creativity meter</label>
+
+            <input
+              type="range"
+              min="1"
+              max="100"
+              value={creativityMeter}
+              class="slider"
+              id="Creativity input"
+              onChange={(e) => setCreativityMeter(e.target.value)}
+            />
+            <div className={s.content_right_meter} />
+          </div>
           <div className={s.content_right_bottom}>
             <div className={s.nothing_attached_text}>
               Nothing currently attached
